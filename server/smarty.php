eb7c7ca9d5b59b1262f7ecbcf821f1fcd3179bef
trishan <trishandel@gmail.com>
2010-10-09 15:13:51 -0400

    consolidated AppTrac installation configuration settings

--- server/smarty.php
+++ server/smarty.php
@@ -1,9 +1,10 @@
 <?php
+$conf = parse_ini_file("config.ini.php");
 
 require_once("Smarty-2.6.26/libs/Smarty.class.php");
 $smarty = new Smarty();
 
-$wwwroot = "/home/aalcorn/www/litlab";
+$wwwroot = $conf["wwwroot"];
 
 $smarty->template_dir = "$wwwroot/templates";
 $smarty->compile_dir  = "$wwwroot/cache/compile";
